f4821ff6957554b788dabd39b0b15f46e1800982
Pablo L Basanta <basanta79@gmail.com>
2019-05-09 23:25:59 +0200

    :sparkles: login form in oldfashioned way

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login as django_login
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
@@ -6,6 +6,8 @@ from django.contrib import messages
 # Create your views here.
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
         username = request.POST['usr']
         password = request.POST['pwd']
@@ -16,3 +18,9 @@ def login(request):
             django_login(request, user)
             return redirect('home')
     return render(request, 'users/login.html')
+
+
+def logout(request):
+    django_logout(request)
+    return redirect('login')
+
